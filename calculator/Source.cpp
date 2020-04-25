228777344f614178b0b0df983a5bbed7600181d0
zukaberidze <zaqaria.beridze@sangu.edu.ge>
2020-04-25 10:54:56 +0400

    second one

--- calculator/Source.cpp
+++ calculator/Source.cpp
@@ -6,6 +6,12 @@ int main()
     char op;
     float num1, num2;
 
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
     switch (op)
     {
     case '+':
